3a25308f7b27a5f6dcc17eb1b3069f6a695534d3
David Evans <david0evans@gmail.com>
2014-03-31 19:47:25 -0700

    ugh

--- src/Player.js
+++ src/Player.js
@@ -45,11 +45,11 @@ TANK.registerComponent("Player")
     {
       var scale = Math.min(e.scale, 1.1);
       scale = Math.max(scale, 0.9);
-      TANK.RenderManager.camera.z = TANK.RenderManager.camera.z * scale;
+      TANK.RenderManager.camera.z *= scale;
       if (TANK.RenderManager.camera.z < 1)
         TANK.RenderManager.camera.z = 1;
-      if (TANK.RenderManager.camera.z > 10)
-        TANK.RenderManager.camera.z = 10;
+      if (TANK.RenderManager.camera.z > 100)
+        TANK.RenderManager.camera.z = 100;
 
     }
   });
