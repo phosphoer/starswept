a9807a8087ca9212ff8855c02b44905107fb0c0d
David Evans <david0evans@gmail.com>
2014-03-31 19:46:51 -0700

    hm

--- src/Player.js
+++ src/Player.js
@@ -45,8 +45,7 @@ TANK.registerComponent("Player")
     {
       var scale = Math.min(e.scale, 1.1);
       scale = Math.max(scale, 0.9);
-      window.document.title = scale;
-      TANK.RenderManager.camera.z = Math.sqrt(TANK.RenderManager.camera.z * scale);
+      TANK.RenderManager.camera.z = TANK.RenderManager.camera.z * scale;
       if (TANK.RenderManager.camera.z < 1)
         TANK.RenderManager.camera.z = 1;
       if (TANK.RenderManager.camera.z > 10)
