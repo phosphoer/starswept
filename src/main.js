09d00abae3b5e9288c0e4768f92c4dc1718219aa
David Evans <david0evans@gmail.com>
2014-07-21 22:12:52 -0700

    Parallax dust field

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 function main()
 {
-  TANK.createEngine(["Input", "Renderer2D", "Game", "StarField"]);
+  TANK.createEngine(["Input", "Renderer2D", "Game", "StarField", "DustField"]);
 
   TANK.main.Renderer2D.context = document.querySelector("#canvas").getContext("2d");
   TANK.main.Input.context = document.querySelector("#stage");
