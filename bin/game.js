a764fe41ae694a6015f99cd2d37dfb6d961fa722
David Evans <david0evans@gmail.com>
2014-08-15 11:27:45 -0700

    Finish map generation with island detection

--- bin/game.js
+++ bin/game.js
@@ -1033,6 +1033,8 @@ TANK.registerComponent("CampaignMap")
   this.zdepth = 0;
   this.size = 400;
   this.minDist = 100;
+  this.systems = [];
+  this.islands = [];
 })
 
 .initialize(function()
@@ -1040,93 +1042,153 @@ TANK.registerComponent("CampaignMap")
   var that = this;
   TANK.main.Renderer2D.add(this);
 
-  this.systems = [];
-
-  // Generate systems
-  for (var i = 0; i < 10; ++i)
+  this.generateMap = function()
   {
-    this.systems.push(
-    {
-      level: Levels[0],
-      pos: 
-      [
-        -this.size + Math.random() * this.size * 2, 
-        -this.size + Math.random() * this.size * 2
-      ],
-      radius: 20,
-      edges: [],
-      owned: false
-    });
+    this.systems = [];
 
-    // Ensure systems are far apart
-    var system = this.systems[i];
-    var angle = Math.random() * Math.PI * 2;
-    for (;;)
+    // Generate systems
+    for (var i = 0; i < 10; ++i)
     {
-      var minDist = this.systems.reduce(function(prev, cur)
+      this.systems.push(
       {
-        if (cur === system)
-          return prev;
-        return Math.min(prev, TANK.Math2D.pointDistancePoint(system.pos, cur.pos));
-      }, Infinity);
+        level: Levels[0],
+        pos:
+        [
+          -this.size + Math.random() * this.size * 2,
+          -this.size + Math.random() * this.size * 2
+        ],
+        radius: 20,
+        edges: [],
+        owned: false
+      });
 
-      if (minDist > this.minDist)
-        break;
-
-      system.pos[0] += Math.cos(angle) * 10;
-      system.pos[1] += Math.sin(angle) * 10;
-    }
-  }
-
-  // Make edges
-  function makeEdgesFromNode(node, visited)
-  {
-    visited.push(node);
-    var searchRadius = 100;
-    var searching = true;
-    while (searching)
-    {
-      for (var i = 0; i < that.systems.length; ++i)
+      // Ensure systems are far apart
+      var system = this.systems[i];
+      var angle = Math.random() * Math.PI * 2;
+      for (;;)
       {
-        var system = that.systems[i];
-        if (system === node)
-          continue;
-
-        var dist = TANK.Math2D.pointDistancePoint(node.pos, system.pos);
-        if (dist < searchRadius && visited.indexOf(system) < 0)
+        var minDist = this.systems.reduce(function(prev, cur)
         {
-          node.edges.push(system);
-          system.edges.push(node);
-          makeEdgesFromNode(system, visited);
-          return;
+          if (cur === system)
+            return prev;
+          return Math.min(prev, TANK.Math2D.pointDistancePoint(system.pos, cur.pos));
+        }, Infinity);
+
+        if (minDist > this.minDist)
+          break;
+
+        system.pos[0] += Math.cos(angle) * 10;
+        system.pos[1] += Math.sin(angle) * 10;
+      }
+    }
+
+    // Make edges
+    this.systems.forEach(function(system)
+    {
+      // Get systems in distance order
+      var systemsOrdered = this.systems.slice();
+      systemsOrdered.sort(function(a, b)
+      {
+        var distA = TANK.Math2D.pointDistancePoint(system.pos, a.pos);
+        var distB = TANK.Math2D.pointDistancePoint(system.pos, b.pos);
+        return distA > distB;
+      });
+      systemsOrdered.splice(0, 1);
+
+      // Connect to closest system
+      var closest = systemsOrdered[0];
+      system.edges.push(closest);
+      closest.edges.push(system);
+      systemsOrdered.splice(0, 1);
+
+      // Find the next closest system that is not inline with the previous
+      var closestVec = TANK.Math2D.normalize(TANK.Math2D.subtract(closest.pos, system.pos));
+      var nextClosest = null;
+      for (var i = 0; i < systemsOrdered.length; ++i)
+      {
+        var s= systemsOrdered[i];
+        var systemVec = TANK.Math2D.normalize(TANK.Math2D.subtract(s.pos, system.pos));
+        var dot = TANK.Math2D.dot(closestVec, systemVec);
+        if (Math.abs(1 - dot) > 0.1)
+        {
+          nextClosest = s;
+          break;
+        }
+      };
+
+      if (nextClosest)
+      {
+        system.edges.push(nextClosest);
+        nextClosest.edges.push(system);
+      }
+
+    }.bind(this));
+
+    // Helper to recursively explore a graph
+    var exploreNode = function(node, islandNodes)
+    {
+      if (node.visited)
+        return false;
+
+      // Visit the node
+      node.visited = true;
+      islandNodes.push(node);
+
+      // Explore each adjacent node
+      for (var i = 0; i < node.edges.length; ++i)
+        exploreNode(node.edges[i], islandNodes);
+
+      return true;
+    };
+
+    // Helper to connect two islands
+    var connectIslands = function(a, b)
+    {
+      // Find the two closest nodes between the islands
+      var minDist = Infinity;
+      var nodeA = null;
+      var nodeB = null;
+
+      for (var i = 0; i < a.length; ++i)
+      {
+        for (var j = 0; j < b.length; ++j)
+        {
+          var dist = TANK.Math2D.pointDistancePoint(a[i].pos, b[j].pos);
+          if (dist < minDist)
+          {
+            minDist = dist;
+            nodeA = a[i];
+            nodeB = b[j];
+          }
         }
       }
-      searchRadius += 10;
 
-      if (visited.length === that.systems.length)
-        return;
+      nodeA.bad = true;
+      nodeB.bad = true;
+      nodeA.edges.push(nodeB);
+      nodeB.edges.push(nodeA);
+    };
+
+    // Explore every node to find islands
+    this.islands = [];
+    for (var i = 0; i < this.systems.length; ++i)
+    {
+      var node = this.systems[i];
+      var island = [];
+      if (exploreNode(node, island))
+        this.islands.push(island);
+    }
+
+    // Connect each island to the next
+    for (var i = 1; i < this.islands.length; ++i)
+    {
+      var islandA = this.islands[i - 1];
+      var islandB = this.islands[i];
+      connectIslands(islandA, islandB);
     }
   };
-  makeEdgesFromNode(this.systems[0], []);
 
-  // Move nodes away from edges
-  this.systems.forEach(function(system)
-  {
-    system.edges.forEach(function(systemB)
-    {
-      this.systems.forEach(function(s)
-      {
-        if (s === system || s === systemB)
-          return;
-
-        var dist = TANK.Math2D.pointDistanceLine(s.pos, system.pos, systemB.pos, true);
-        if (dist < 50)
-        {
-          s.bad = true;
-        }
-      }.bind(this));
-    }.bind(this));
-  }.bind(this));
+  this.generateMap();
 
   this.listenTo(TANK.main, 'mousedown', function(e)
   {
