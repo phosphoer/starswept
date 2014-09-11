7193470ec2f59be7b40b2af49401d8e3b3769436
David Evans <david0evans@gmail.com>
2014-09-10 20:32:55 -0700

    Update TANK

--- bin/game.js
+++ bin/game.js
@@ -3937,7 +3937,7 @@ function main()
 {
   LoadSounds();
 
-  TANK.createEngine(['Input', 'Renderer2D', 'Resources', 'Game', 'MapGeneration', 'StarField', 'DustField']);
+  TANK.createEngine(['Input', 'CollisionManager', 'Renderer2D', 'Resources', 'Game', 'MapGeneration', 'StarField', 'DustField']);
 
   TANK.main.Renderer2D.context = document.querySelector('#canvas').getContext('2d');
   TANK.main.Input.context = document.querySelector('#stage');
