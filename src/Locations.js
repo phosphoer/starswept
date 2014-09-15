082249be0fb8715c0b5843fc779aa4e2b1301f9c
David Evans <david0evans@gmail.com>
2014-09-14 19:48:03 -0700

    reformat locations

--- src/Locations.js
+++ src/Locations.js
@@ -17,7 +17,11 @@ Locations.abandonedOutpost =
 {
   text: 'A dingy trading outpost sits ahead, listing heavily to the side.',
   name: 'An old abandoned trading outpost',
-  events: [{probability: 0.5, name: 'pirate'}, {probability: 0.5, name: 'derelict'}],
+  events:
+  [
+    {probability: 0.5, name: 'pirate'},
+    {probability: 0.5, name: 'derelict'}
+  ],
   bgColor: [0, 20, 0, 1],
   lightColor: [0.8, 1, 0.8],
   lightDir: Math.PI * 2 * 0.5,
@@ -32,7 +36,11 @@ Locations.researchStation =
 {
   text: 'The research station looks like it hasn\'t been visited in years.',
   name: 'A research station',
-  events: [{probability: 0.5, name: 'pirate'}, {probability: 0.5, name: 'derelict'}],
+  events:
+  [
+    {probability: 0.5, name: 'pirate'},
+    {probability: 0.5, name: 'derelict'}
+  ],
   bgColor: [20, 20, 0, 1],
   lightColor: [1, 1, 0.8],
   lightDir: Math.PI * 2 * 0.2,
@@ -47,22 +55,36 @@ Locations.pirateBase =
 {
   text: 'Only pirates continue to make their home near the galaxy\'s center. You feel extremely nervous.',
   name: 'A pirate outpost',
-  events: [{probability: 0.75, name: 'pirate'}, {probability: 0.25, name: 'empty'}],
+  events:
+  [
+    {probability: 0.75, name: 'pirate'},
+    {probability: 0.25, name: 'empty'}
+  ],
   bgColor: [0, 20, 20, 1],
   lightColor: [0.8, 1, 1],
   lightDir: Math.PI * 2 * 0.9,
-  spawns: [{components: {Clouds: {numClouds: 30, cloudColor: [180, 255, 255]}}}]
+  spawns:
+  [
+    {components: {Clouds: {numClouds: 30, cloudColor: [180, 255, 255]}}}
+  ]
 };
 
 Locations.oldBattlefield =
 {
   text: 'This system is littered with the wrecks of ancient warships.',
   name: 'An old battlefield',
-  events: [{probability: 0.5, name: 'pirate'}, {probability: 0.5, name: 'empty'}],
+  events:
+  [
+    {probability: 0.5, name: 'pirate'},
+    {probability: 0.5, name: 'empty'}
+  ],
   bgColor: [0, 20, 20, 1],
   lightColor: [0.8, 1, 1],
   lightDir: Math.PI * 2 * 0.9,
-  spawns: [{components: {Clouds: {numClouds: 30, cloudColor: [180, 255, 255]}}}]
+  spawns:
+  [
+    {components: {Clouds: {numClouds: 30, cloudColor: [180, 255, 255]}}}
+  ]
 };
 
 Locations.deepSpace =
@@ -89,11 +111,16 @@ Locations.asteroidField =
 {
   text: 'Here in the depths of an asteroid field, anything can happen. Watch your back.',
   name: 'Asteroid field',
-  events: [{probability: 0.4, name: 'pirate'}, {probability: 0.6, name: 'derelict'}],
+  events:
+  [
+    {probability: 0.4, name: 'pirate'},
+    {probability: 0.6, name: 'derelict'}
+  ],
   bgColor: [30, 0, 0, 1],
   lightColor: [1, 0.7, 0.7],
   lightDir: Math.PI * 2 * 0.2,
-  spawns: [
+  spawns:
+  [
     {components: {Clouds: {numClouds: 75, cloudColor: [220, 180, 180]}}},
     {components: {AsteroidField: {numAsteroids: 40}}}
   ]
