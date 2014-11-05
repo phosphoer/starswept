0728e093c849f3d7c21b4ec7d6cf7e3f08800fca
David Evans <david0evans@gmail.com>
2014-11-04 19:30:43 -0800

    Update ship indicator graphics

--- bin/game.js
+++ bin/game.js
@@ -4667,13 +4667,17 @@ TANK.registerComponent('ShipIndicator')
     var screenPos = [t.x - camera.x, t.y - camera.y];
     var cameraTopLeft = TANK.Math2D.scale([-window.innerWidth / 2, -window.innerHeight / 2], camera.z);
     var cameraBottomRight = TANK.Math2D.scale([window.innerWidth / 2, window.innerHeight / 2], camera.z);
-    var onScreen = true;
-    if (screenPos[0] < cameraTopLeft[0] || screenPos[1] < cameraTopLeft[1])
-      onScreen = false;
-    if (screenPos[0] > cameraBottomRight[0] || screenPos[1] > cameraBottomRight[1])
-      onScreen = false;
+    var indicatorAngle = null;
+    if (screenPos[0] < cameraTopLeft[0])
+      indicatorAngle = Math.PI;
+    if (screenPos[1] < cameraTopLeft[1])
+      indicatorAngle = Math.PI / -2;
+    if (screenPos[0] > cameraBottomRight[0])
+      indicatorAngle = 0;
+    if (screenPos[1] > cameraBottomRight[1])
+      indicatorAngle = Math.PI / 2;
 
-    if (!onScreen)
+    if (indicatorAngle !== null)
     {
       ctx.save();
       screenPos[0] = Math.max(cameraTopLeft[0], screenPos[0]);
@@ -4681,13 +4685,26 @@ TANK.registerComponent('ShipIndicator')
       screenPos[0] = Math.min(cameraBottomRight[0], screenPos[0]);
       screenPos[1] = Math.min(cameraBottomRight[1], screenPos[1]);
 
-      if (ship.iff === TANK.main.Game.player.Ship.iff)
-        ctx.fillStyle = 'rgba(0, 255, 0, 0.5)';
-      else
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+      ctx.translate(screenPos[0], screenPos[1]);
+      ctx.rotate(indicatorAngle);
+      ctx.scale(camera.z, camera.z);
+
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+
+      if (ship)
+      {
+        if (ship.iff === TANK.main.Game.player.Ship.iff)
+          ctx.fillStyle = 'rgba(0, 255, 0, 0.5)';
+        else
+          ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+      }
 
       ctx.beginPath();
-      ctx.arc(screenPos[0], screenPos[1], 30 * camera.z, Math.PI * 2, false);
+      ctx.moveTo(0, 0);
+      ctx.lineTo(-40, -20);
+      ctx.lineTo(-30, 0);
+      ctx.lineTo(-40, 20);
+      ctx.lineTo(0, 0);
       ctx.closePath();
       ctx.fill();
       ctx.restore();
